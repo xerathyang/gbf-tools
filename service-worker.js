a28be2770a0ca5703455883acef075dff5203c23
xerathyang <c4940578@gmail.com>
2023-03-10 05:19:48 +0000

    Deploying to gh-pages from @ xerathyang/gbf-tools@c9835d218215cdf714516c571a1d98476bc9c14f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.43fd98b0ec461a04172f9a6d905ff879.js"
+  "/gbf-tools/precache-manifest.be39a06d5883ccf3ddb0f006c1432900.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gbf-tools/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
